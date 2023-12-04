557188cb8a6a998eef82d106c74428aa65cf39af
Mateusz <mateusz.widawski@onet.pl>
2023-12-04 15:06:56 +0100

    Added to cart information

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -33,7 +33,7 @@ products.forEach((product) => {
 
     <div class="product-spacer"></div>
 
-    <div class="added-to-cart">
+    <div class="added-to-cart js-added-to-cart-${product.id}">
       <img src="images/icons/checkmark.png" />
       Added
     </div>
@@ -45,8 +45,9 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  let addedMessageTimeoutId;
   button.addEventListener('click', () => {
-    const productId = button.dataset.productId;
+    const { productId } = button.dataset;
 
     let matchingItem;
 
@@ -56,14 +57,14 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       }
     });
 
-    let quantityValue = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+    let quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
 
     if (matchingItem) {
-      matchingItem.quantity += quantityValue;
+      matchingItem.quantity += quantity;
     } else {
       cart.push({
-        productId: productId,
-        quantity: quantityValue,
+        productId,
+        quantity,
       });
     }
 
@@ -73,5 +74,18 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 
     document.querySelector('.cart-quantity').innerHTML = cartQuantity;
+
+    const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+    addedMessage.classList.add('added-to-cart-visible');
+
+    if (addedMessageTimeoutId) {
+      clearTimeout(addedMessageTimeoutId);
+    }
+
+    const timeoutId = setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000);
+
+    addedMessageTimeoutId = timeoutId;
   });
 });
